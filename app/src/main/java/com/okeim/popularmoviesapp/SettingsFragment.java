c41f6641d49f158252c25282cd32e535494814bc
burrowqueen <moc.mieko@okeim.com>
2016-03-19 21:19:34 -0500

    Rewrite SettingsActivity to use PreferenceFragment

---
+++ app/src/main/java/com/okeim/popularmoviesapp/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.okeim.popularmoviesapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ *
+ */
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+}
